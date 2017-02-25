5cc526f6bb09a44de1a94a4491b9d0a79d089e5b
Jee Hoon <yamoo9@naver.com>
2017-02-25 17:07:00 +0900

    JavaScript 웹 브라우저에서의 실행 흐름 이해/실습

---
+++ LECTURE/DAY01/js/study-js.js
@@ -0,0 +1,30 @@
+// 웹 브라우저 환경에서 JavaScript의 처리 흐름
+
+// 직렬(Sync) 프로세스
+// A -> B -> C
+// 병렬(Async) 프로세스
+// A -> A+
+//   B -> B-
+
+// h1#heading 요소에 JavaScript로 접근하여
+// var -> 변수(variable) 선언 식별 키워드
+var heading = document.querySelector('#heading');
+console.log(heading); // null "비어 있다"
+
+// 제목 요소의 글자 색상을 변경한다.
+heading.style.color = 'hotpink'; // Error
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
